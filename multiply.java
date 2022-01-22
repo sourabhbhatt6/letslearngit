2ddd5f1543d4ac01814093bed3d6db3d580a52ad
srb <srb2636@gmail.com>
2022-01-22 16:45:29 +0530

    added multiplu.java

---
+++ multiply.java
@@ -0,0 +1,11 @@
+public class multiply 
+{
+    public static void main(String[] args)
+    {
+        int a=2;
+        int b=4;
+
+        system.out.println("the product is"+ (a*b));
+        
+    }
+}
